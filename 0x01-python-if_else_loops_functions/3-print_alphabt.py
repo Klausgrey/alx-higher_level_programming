b9c34cf72b0678cd10ea5aa78e06727a426557d4
Nicholas <danielnick210@gmail.com>
2023-11-28 21:48:12 +0100

    3--print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in "abcdefghijklmnopqrstuvwxyz":
-    if i == 'q' and i == 'e':
-        continue
-    print("{}".format(i), end="")
+for i in range(ord('a'), ord("z")+1):
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end="")
