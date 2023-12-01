7ac195ec2bf1c5ad525de0a848223ee6605b1c17
Nicholas <danielnick210@gmail.com>
2023-12-01 18:19:47 +0100

    2-argss.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    x = len(argv) - 1
+    if x == 0:
+        print("{}: arguments".format(x))
+    elif x == 1:
+        print("{}: arguments".format(x))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{}: arguments".format(x))
+        i = 1
+        for ars in argv[1:]:
+            print("{}: {}".format(i, ars))
+            i += 1
