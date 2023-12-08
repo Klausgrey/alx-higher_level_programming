bb4ce5284f367e4671a9bd9aa91f951b7c7ceef7
Nicholas <danielnick210@gmail.com>
2023-12-08 15:58:40 +0100

    roman numerails

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    if not isinstance(roman_string,str):
+        return 0
+    romanValue = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    totalValue = 0
+    previousValue = 0
+    for i in roman_string:
+        currentValue = romanValue[i]
+        totalValue += currentValue
+        if currentValue > previousValue:
+            totalValue -= 2 * previousValue
+        previousValue = currentValue
+    return totalValue
