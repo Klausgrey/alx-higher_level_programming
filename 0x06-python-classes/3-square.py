745de4cfbda094fbf5d60c104163c53470524f7c
Nicholas <danielnick210@gmail.com>
2024-02-06 11:43:04 +0100

    3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python37
+
+
+''' Defines a square class '''
+
+
+class Square:
+    """ This is a square class checks if the size is an integer or is < 0 """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
