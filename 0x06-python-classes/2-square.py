eac8a1dbe8bfba0924076b63c1f17d90846299b4
Nicholas <danielnick210@gmail.com>
2024-02-06 11:22:11 +0100

    bug fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,10 @@
 class Square:
     """ This is a square class checks if the size is an integer or is < 0 """
     def __init__(self, size=0):
-        self.size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+        self.size = size
 
