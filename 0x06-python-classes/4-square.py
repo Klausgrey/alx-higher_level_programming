8f1033e06d09fd945834d8971d31fed16fbf57b5
Nicholas <danielnick210@gmail.com>
2024-02-06 12:34:40 +0100

    4-square plus some improvements

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python37
+
+''' Defines a square class '''
+
+class Square:
+    """ This is a square class checks if the size is an integer or is < 0 """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Public instance method that returns size """
+        return self.__size
+        
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
