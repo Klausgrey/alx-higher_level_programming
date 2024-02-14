075a5de8d5ff59155b5a25d4f89e3e4b8019f99f
Nicholas <danielnick210@gmail.com>
2024-02-15 00:34:38 +0100

    bug fix

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- selects all cities of california found in the database hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id IN (
+	SELECT id FROM states WHERE name = 'California')
+ORDER BY id;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- lists all cities contained in the hbtn_0d_usa database
+SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s
+ON c.state_id = s.id ORDER BY c.id;
